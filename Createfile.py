4c84dff28c4c46d786612ded6a700f832be9bfe9
vimal130795 <vimal.vijayasekar@outlook.com>
2019-05-08 11:37:22 +0530

    script with static data

---
+++ Createfile.py
@@ -0,0 +1,18 @@
+import os
+files = ['New.Text_Document.txt','New_Text_Document.txt','New Text Document.txt','New.Text_Document.txt','New Text Document.txt','New.Text_Document.txt']
+for a in files:
+    temp = a
+    i=1
+    while 1:
+        if os.path.exists(temp):
+            b = a.split('.')
+            b[-2] = b[-2]+' ('+str(i)+')'
+            temp ='.'.join(b)
+            i+=1
+        else:
+            a = temp
+            break
+    print (a)
+    with open(a,'w') as f:
+        f.write('hello')
+
